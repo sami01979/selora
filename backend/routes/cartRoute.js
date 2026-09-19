54cd000a05a3ea72a36ffb748f5fe6b1ac5c143c
Shakhawat Sami <git config --global user.email "shakhawat5ap22@gmail.com>
2026-09-19 16:52:38 +0600

    updated key

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -4,8 +4,10 @@ import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
+
 cartRouter.post("/get", authUser, getUserCart);
 cartRouter.post("/add", authUser, addToCart);
 cartRouter.post("/update", authUser, updateCart);
+
 
 export default cartRouter;
